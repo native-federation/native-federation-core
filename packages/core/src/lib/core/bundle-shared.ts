85a9750824b4d4c30ddd7e420660509d35eab7cd
Aukevanoost <auke.v.o@hotmail.com>
2026-03-09 09:57:02 +0100

    fix: Removed redundant empty chunk lists

--- packages/core/src/lib/core/bundle-shared.ts
+++ packages/core/src/lib/core/bundle-shared.ts
@@ -158,7 +158,9 @@ export async function bundleShared(
     result.forEach(external => {
       external.bundle = buildOptions.bundleName;
     });
-    exportedChunks = { [buildOptions.bundleName]: getChunkFileNames(chunks) };
+    if (chunks.length > 0) {
+      exportedChunks = { [buildOptions.bundleName]: getChunkFileNames(chunks) };
+    }
   } else {
     addChunksToResult(chunks, result);
   }
